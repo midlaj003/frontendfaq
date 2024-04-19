97101752df025c633b1106612120c7348366e0df
midlaj003 <129929395+midlaj003@users.noreply.github.com>
2024-04-20 03:55:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var acc = document.getElementsByClassName('question')
+
+// Display the first answer as block
+acc[0].nextElementSibling.style.display = 'block';
+// Update the icon of the first question
+var icon = acc[0].querySelector('.plus-icon');
+icon.src = './assets/images/icon-minus.svg';
+
 Array.from(acc).forEach(question => {
     question.addEventListener('click', () => {
         const answer = question.nextElementSibling;
